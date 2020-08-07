9de6b21e97e7db83d19127247f5b7cda46412f7d
Erik Scow <erikscow@gmail.com>
2020-08-06 20:05:31 -0700

    Completed all endpoints.

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const bcrypt = require('bcryptjs')
+const session = require('express-session')
 
 const db = require('./db-config')
 
 const server = express()
 
 server.use(express.json())
+server.use(
+    session({
+        name: 'the session',
+        secret: 'its a secret',
+        cookie: {
+            maxAge: 1000 * 60 * 60,
+            secure: false,
+            httpOnly: true,
+        },
+        resave: false,
+        saveUninitialized: false
+    })
+)
 
 server.post('/api/register', (req, res) => {
     const credentials = req.body
@@ -28,6 +42,7 @@ server.post('/api/login', (req, res) => {
         .first()
         .then(user => {
             if (user && bcrypt.compareSync(password, user.password)) {
+                req.session.user = user
                 res.status(200).json({message: "successful login"})
             } else {
                 res.status(401).json({message: "invalid credentials"})
@@ -40,9 +55,24 @@ server.post('/api/login', (req, res) => {
     
 })
 
-server.get('/api/users', (req, res) => {
-
+server.get('/api/users', validateSession, (req, res) => {
+    db('users')
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({message: "could not get from database"})
+        })
 })
+
+function validateSession(req, res, next){
+    if (req.session && req.session.user){
+        next()
+    } else {
+        res.status(401).json({message: "invalid credentials"})
+    }
+}
 
 const port = process.env.PORT || 5000
 
